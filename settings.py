6d750b9b56b0da3b53d4f3ec0086b504293f3c6c
imouren <mouren11@163.com>
2013-01-22 14:25:08 +0800

    settings

--- settings.py
+++ settings.py
@@ -45,8 +45,8 @@ SECRET_KEY = '2k4aa7q-@!%m*4da2p2@9qzg=-4lr7)w(b2!yh1zvaq_=9&'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
