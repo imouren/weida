f60736ec2c763cbc1156409d05d434b344861d69
imouren <mouren11@163.com>
2013-01-22 16:03:11 +0800

    u

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,7 +2,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('apps.users.views',
-    (r'^$', 'user_login'),
     (r'^logout/$', 'user_logout'),
     (r'^login/$', 'user_login'),
     (r'^register/$', 'register'),
